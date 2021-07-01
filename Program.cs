74f957048b41f91040d7f925a0a77a256efcb65a
mszymaczek <michal.szymaczek@gmail.com>
2021-07-01 13:37:48 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moja_pierwsza_gra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej, to moja pierwsza apka!");
+            Console.ReadLine();
+        }
+    }
+}
